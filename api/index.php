c5795804e23a4ae4d0977254f4a0e7d032901165
tri endriardi juliansyah <54131009+oxyrus69@users.noreply.github.com>
2025-12-11 09:29:12 +0700

    adding comments

--- api/index.php
+++ api/index.php
@@ -14,24 +14,23 @@ $app = require_once __DIR__ . '/../bootstrap/app.php';
 |--------------------------------------------------------------------------
 | VERCEL FIX: Read-Only Filesystem
 |--------------------------------------------------------------------------
-| Karena Vercel read-only, kita pindahkan storage path ke folder "/tmp"
+| Karena Vercel read-only, pindahkan storage path ke folder "/tmp"
 | yang diizinkan untuk ditulis.
 */
 $storage = '/tmp/storage';
 
 if (!file_exists($storage)) {
-    // Buat folder struktur storage di /tmp secara otomatis
+
     mkdir($storage . '/framework/views', 0777, true);
     mkdir($storage . '/framework/cache', 0777, true);
     mkdir($storage . '/framework/sessions', 0777, true);
     mkdir($storage . '/logs', 0777, true);
 }
 
-// Perintahkan Laravel pakai folder storage di /tmp
+
 $app->useStoragePath($storage);
 
-// Jalankan Aplikasi
-// (Gunakan handleRequest untuk Laravel 11, atau handle untuk versi lama)
+// handle error
 if (method_exists($app, 'handleRequest')) {
     $app->handleRequest(Request::capture());
 } else {
