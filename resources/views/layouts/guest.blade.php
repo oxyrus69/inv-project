14596352257332d99e06eb4908f85f7342f97841
tri endriardi juliansyah <54131009+oxyrus69@users.noreply.github.com>
2025-12-11 09:37:50 +0700

    adding alpine

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,6 +14,7 @@
 
     <!-- Scripts -->
     <script src="https://cdn.tailwindcss.com"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.3/dist/cdn.min.js"></script>
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
